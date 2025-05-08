d10ae700819f4b215fa76eb01b4b0959bf766011
gmartincor <guillermomc007@gmail.com>
2025-05-08 17:45:26 +0200

    mail

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,17 +1,8 @@
-require 'mailgun-ruby'
-
-api_key = ENV['MAILGUN_API_KEY']
-domain = ENV['MAILGUN_DOMAIN']
-
-if api_key.nil? || domain.nil?
+if ENV['MAILGUN_API_KEY'].present? && ENV['MAILGUN_DOMAIN'].present?
+  Rails.application.config.action_mailer.mailgun_settings = {
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain: ENV['MAILGUN_DOMAIN']
+  }
+else
   Rails.logger.error("⚠️ MAILGUN_API_KEY o MAILGUN_DOMAIN no configurados. El envío de correo no funcionará.")
 end
-
-Mailgun.configure do |config|
-  config.api_key = api_key if api_key
-end
-
-ActionMailer::Base.add_delivery_method(:mailgun, Mailgun::DeliveryMethod, 
-  api_key: api_key, 
-  domain: domain
-) if api_key && domain
